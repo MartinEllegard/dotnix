4ede506a1727f5b17aa517913e117a868fe32334
Martin Ellegård <martin.ellegard@gmail.com>
2024-11-11 00:18:57 +0100

    Cleaned up home file a bit

--- home.nix
+++ home.nix
@@ -1,17 +1,5 @@
 { config, pkgs, inputs, ... }:
-# let
-#   home-manager-shared = inputs.shared-flak;
-#   #   builtins.fetchGit {
-#   #   url = "https://github.com/MartinEllegard/home-manager-shared";
-#   #   rev = "b62717f96406530d414c60ce1cf16d0c619215f0";
-#   # };
-# in
 {
-  # imports = [
-  #   (inputs.shared-flake + "/modules")
-  # ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "martin";
   home.homeDirectory = "/home/martin";
 
@@ -24,17 +12,9 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  # Install programs 
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-
     # IMPORTANT: These need to be downloaded by system
-    # pkgs.hyprland
-    # pkgs.hyprpaper
-    # pkgs.hyprlock
-    # pkgs.hyprcursor
     # pkgs.mako
     # pkgs.wl-clipboard
     # pkgs.xclip
@@ -42,44 +22,26 @@
 
     pkgs.gruvbox-gtk-theme
     pkgs.gruvbox-plus-icons
-    pkgs.wezterm
-    #pkgs.uv
-    #pkgs.insomnia
-    #pkgs.ripgrep
     pkgs.beekeeper-studio
 
     # Create nerd font
     (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   programs = {
-    # hyprland.enable = true;
-    # hyprpaper.enable = true;
-    # hyprlock.enable = true;
-    # hyprcursor.enable = true;
     git = {
       enable = true;
       userName = "Martin Ellegård";
       userEmail = "martin.ellegard@gmail.com";
     };
-    #fish.enable = true;
   };
 
   # environment.shells = [
   #   pkgs.fish
   # ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # Import dotfiles/configs
   home.file = {
-
     ".config/hypr".source = ./dotfiles/hypr;
     ".config/waybar".source = ./dotfiles/waybar;
     ".config/mako".source = ./dotfiles/mako;
@@ -91,37 +53,6 @@
 
     ".config/gtk-3.0".source = ./dotfiles/gtk-3.0;
     ".config/gtk-4.0".source = ./dotfiles/gtk-4.0;
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/martin/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
